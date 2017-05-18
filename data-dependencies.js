c4fec79c419b038ff85460ac11c8ba45b4caed3b
Bert Spaan <bert.spaan@gmail.com>
2017-05-18 14:02:42 -0400

    use viz.js to create svg

--- data-dependencies.js
+++ data-dependencies.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const path = require('path')
+const Viz = require('viz.js')
+const cheerio = require('cheerio')
 const orchestrator = require('@spacetime/orchestrator')
 
 function aggregate (config, dirs, tools, callback) {
@@ -7,7 +9,23 @@ function aggregate (config, dirs, tools, callback) {
     useSteps: false
   }).getDot()
 
+  const svg = Viz(dot, {
+    format: 'svg'
+  })
+
+  const $ = cheerio.load(svg)
+
+  // Remove polygons with white backgroundf
+  $('polygon[fill=#ffffff]').remove()
+
+  $('.node').map((index, element) => {
+    const dataset = $('text', element).html()
+    const link = $(`<a xlink:href="https://github.com/nypl-spacetime/etl-${dataset}"></a>`)
+    return $(element).wrap(link)
+  })
+
   fs.writeFileSync(path.join(dirs.current, 'spacetime.dot'), dot)
+  fs.writeFileSync(path.join(dirs.current, 'spacetime.svg'), $.html())
 
   callback()
 }
